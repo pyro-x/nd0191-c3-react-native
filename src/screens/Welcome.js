02b254a82421c20b6c2bb42fc696df2901ab503b
David Monterroso Cabello <david.monterroso@tuispain.com>
2023-02-02 21:26:20 +0100

    began to create the basic structure and dependenc

---
+++ src/screens/Welcome.js
@@ -0,0 +1,11 @@
+
+const Welcome = () => {
+    return (
+        <View style={styles.container}>
+        <Text style={styles.text}>Welcome</Text>
+        </View>
+    );
+};
+
+
+export default Welcome;
